8c6b68ec55783443a84ce4911cf030ec74855052
Immanuel Kolapo <immanuelcaspian@gmail.com>
2021-07-06 03:45:37 +0100

    feature: register ChatRoom in admin site

--- woice/chat/admin.py
+++ woice/chat/admin.py
@@ -1,3 +1,9 @@
 from django.contrib import admin
 
-# Register your models here.
+from .models import ChatRoom
+
+
+@admin.register(ChatRoom)
+class ChatRoomAdmin(admin.ModelAdmin):
+    list_display = ["id", "name", "creator", "type", "created", "updated"]
+    list_filter = ["creator", "type"]
