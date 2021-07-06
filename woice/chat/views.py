e313b5fabbeaa1c38b099522c229a4eecff33737
Immanuel Kolapo <immanuelcaspian@gmail.com>
2021-07-06 19:57:19 +0100

    feature: implement make_admin() in ChatRoomView

--- woice/chat/views.py
+++ woice/chat/views.py
@@ -5,11 +5,12 @@ from rest_framework.response import Response
 from rest_framework.viewsets import ModelViewSet
 from templated_email import send_templated_mail
 
-from .models import ChatRoom, RoomType
+from .models import ChatRoom, ChatRoomMember, RoomType
 from .permissions import ChatRoomPermission
 from .serializers import (
     ChatRoomCreateSerializer,
     ChatRoomSerializer,
+    MakeAdminSerializer,
     PrivateChatRoomInviteSerializer,
 )
 
@@ -20,6 +21,7 @@ class ChatRoomViewSet(ModelViewSet):
     serializer_action_classes = {
         "create": ChatRoomCreateSerializer,
         "invite": PrivateChatRoomInviteSerializer,
+        "make_admin": MakeAdminSerializer,
     }
     permission_action_classes = {}
 
@@ -74,6 +76,100 @@ class ChatRoomViewSet(ModelViewSet):
             },
             status=status.HTTP_200_OK,
         )
+
+    @action(detail=False, methods=["post"])
+    def make_admin(self, request, *args, **kwargs):
+        serializer = self.get_serializer(data=request.data)
+        serializer.is_valid(raise_exception=True)
+
+        user_id = serializer.validated_data.get("user_id")
+        make_admin = serializer.validated_data.get("make_admin")
+        chatroom_id = serializer.validated_data.get("chatroom_id")
+
+        try:
+            chatroom = ChatRoom.objects.get(pk=chatroom_id)
+        except ObjectDoesNotExist:
+            return Response(
+                {"status": "error", "message": "Chatroom doesn't exist!"},
+                status=status.HTTP_404_BAD_REQUEST,
+            )
+
+        # Room creator is automatically an admin (see signals). Only run this
+        # check if authenticated user is not the room creator.
+        if request.user != chatroom.creator:
+            try:
+                other_admin = ChatRoomMember.objects.get(
+                    chatroom=chatroom, user=request.user
+                )
+            except ObjectDoesNotExist:
+                return Response(
+                    {
+                        "status": "error",
+                        "message": "You are not a member of this room!",
+                    },
+                    status=status.HTTP_404_NOT_FOUND,
+                )
+
+            if not other_admin.is_admin:
+                return Response(
+                    {
+                        "status": "error",
+                        "message": "You have to be admin to make other users admin!",
+                    },
+                    status=status.HTTP_400_BAD_REQUEST,
+                )
+
+        try:
+            member = ChatRoomMember.objects.get(
+                chatroom=chatroom, user__id=user_id
+            )
+        except ObjectDoesNotExist:
+            return Response(
+                {
+                    "status": "error",
+                    "message": "This user doesn't belong in this room!",
+                },
+                status=status.HTTP_404_NOT_FOUND,
+            )
+
+        if make_admin:
+            if member.is_admin:
+                return Response(
+                    {
+                        "status": "success",
+                        "message": "Member is already an admin",
+                    },
+                    status=status.HTTP_200_OK,
+                )
+            else:
+                member.is_admin = True
+                member.save(update_fields=["is_admin"])
+                return Response(
+                    {
+                        "status": "success",
+                        "message": "Admin rights granted successfully",
+                    },
+                    status=status.HTTP_200_OK,
+                )
+        else:
+            if member.is_admin:
+                member.is_admin = False
+                member.save(update_fields=["is_admin"])
+                return Response(
+                    {
+                        "status": "success",
+                        "message": "Admin rights revoked successfully",
+                    },
+                    status=status.HTTP_200_OK,
+                )
+            else:
+                return Response(
+                    {
+                        "status": "success",
+                        "message": "Member is already not an admin",
+                    },
+                    status=status.HTTP_200_OK,
+                )
 
     def get_permissions(self):
         try:
