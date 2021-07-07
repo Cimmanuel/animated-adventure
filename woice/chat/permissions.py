8b4b77f9649e70e88fdf81efebecc217025c8fde
Immanuel Kolapo <immanuelcaspian@gmail.com>
2021-07-07 02:41:06 +0100

    feature: create AdminPermission

--- woice/chat/permissions.py
+++ woice/chat/permissions.py
@@ -1,4 +1,7 @@
+from django.core.exceptions import ObjectDoesNotExist
 from rest_framework.permissions import BasePermission
+
+from .models import ChatRoom, ChatRoomMember
 
 
 class ChatRoomPermission(BasePermission):
@@ -12,6 +15,37 @@ class ChatRoomPermission(BasePermission):
         if request.user.is_authenticated:
             if view.action == "update":
                 return False
+            else:
+                return True
+        return False
+
+
+class AdminPermission(BasePermission):
+    message = "Admin access only!"
+
+    def has_permission(self, request, view):
+        if request.user.is_authenticated:
+            print(request.user.username)
+            try:
+                chatroom = ChatRoom.objects.get(pk=request.data["chatroom_id"])
+            except ObjectDoesNotExist:
+                return False
+
+            # Room creator is automatically an admin (see signals).
+            # Only run this check if authenticated user is not the
+            # room creator.
+            if request.user != chatroom.creator:
+                try:
+                    other_admin = ChatRoomMember.objects.get(
+                        chatroom=chatroom, user=request.user
+                    )
+                except ObjectDoesNotExist:
+                    return False
+
+                if not other_admin.is_admin:
+                    return False
+                else:
+                    return True
             else:
                 return True
         return False
