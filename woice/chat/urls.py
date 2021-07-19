6fffcfd4cee5ab3263a8934676844be536887ec7
Immanuel Kolapo <immanuelcaspian@gmail.com>
2021-07-19 17:26:04 +0100

    chore: add path to room

--- woice/chat/urls.py
+++ woice/chat/urls.py
@@ -1,9 +1,11 @@
 from django.urls import path
 from rest_framework.routers import DefaultRouter
 
-from .views import ChatRoomViewSet
+from .views import ChatRoomViewSet, room
 
 router = DefaultRouter()
 router.register("room", ChatRoomViewSet, "chatroom")
 
-urlpatterns = [] + router.urls
+urlpatterns = [
+    path("chatroom/<str:pk>/", room, name="room"),
+] + router.urls
