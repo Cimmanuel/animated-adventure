d9fe7e9fd8f5d6541426479b4290276a70719ff3
Immanuel Kolapo <immanuelcaspian@gmail.com>
2021-07-20 14:52:06 +0100

    feature: include route to MembershipSearchView and MessageListView

--- woice/chat/urls.py
+++ woice/chat/urls.py
@@ -1,11 +1,21 @@
 from django.urls import path
 from rest_framework.routers import DefaultRouter
 
-from .views import ChatRoomViewSet, room
+from .views import ChatRoomViewSet, MemberSearchView, MessageListView, room
 
 router = DefaultRouter()
 router.register("room", ChatRoomViewSet, "chatroom")
 
 urlpatterns = [
     path("chatroom/<str:pk>/", room, name="room"),
+    path(
+        "room/<str:pk>/search/",
+        MemberSearchView.as_view(),
+        name="member-search",
+    ),
+    path(
+        "room/<str:pk>/messages/",
+        MessageListView.as_view(),
+        name="messages",
+    ),
 ] + router.urls
