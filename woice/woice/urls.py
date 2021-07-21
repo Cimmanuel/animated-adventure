24ec40a71eafdbc82820405f2c5b4ae2d5dc446e
Immanuel Kolapo <immanuelcaspian@gmail.com>
2021-07-21 02:33:40 +0100

    chore: add path to docs

--- woice/woice/urls.py
+++ woice/woice/urls.py
@@ -15,9 +15,28 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import include, path
+from drf_yasg import openapi
+from drf_yasg.views import get_schema_view
+from rest_framework.permissions import AllowAny
+
+schema_view = get_schema_view(
+    openapi.Info(
+        title="Woice Chat API",
+        default_version="v1",
+        description="Async and live communication",
+        contact=openapi.Contact(email="immanuelcaspian@gmail.com"),
+    ),
+    public=True,
+    permission_classes=[AllowAny],
+)
 
 urlpatterns = [
     path("admin/", admin.site.urls),
     path("accounts/", include("accounts.urls")),
     path("chat/", include("chat.urls")),
+    path(
+        "docs/",
+        schema_view.with_ui("swagger", cache_timeout=0),
+        name="schema-swagger-ui",
+    ),
 ]
